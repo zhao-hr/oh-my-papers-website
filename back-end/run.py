90894d0bd43dc0a4a626900ecfd76775a350e222
zhao-hr <49093503+zhao-hr@users.noreply.github.com>
2021-05-30 03:17:07 +0800

    Update run.py

--- back-end/run.py
+++ back-end/run.py
@@ -2,7 +2,8 @@ import asyncio
 import websockets
 import json
 import os
-from inference_bert import BertInferencer
+import time
+from inference import Inferencer
 import yaml
 
 input = ""
@@ -26,15 +27,32 @@ async def run(websocket, path):
 
 def recommendation(input):
     input = json.loads(input)
-    output = inferencer.inference(input)
+    if input["function"] == 1:
+        input.pop("function", 0)
+        output = inferencer.context_inference(input)
+        output["function"] = 1
+    elif input["function"] == 2:
+        input.pop("function", 0)
+        output = inferencer.citation_inference(input)
+        output["function"] = 2
+    else:
+        input.pop("function", 0)
+        output = inferencer.relation_inference(input)
+        output["function"] = 3
+    #time.sleep(0.01)
     output = json.dumps(output)
+    with open('data/bert_res.json','w+') as f:
+        f.write(output)
     return output
 
 
 if __name__ == '__main__':
-    with open('configs/bert.yaml', 'r') as cfg_file:
-        cfgs = yaml.load(cfg_file, Loader=yaml.FullLoader)
-    inferencer = BertInferencer(**cfgs)
+    with open('configs/citation_bert.yaml', 'r') as cfg_file:
+        engine_cfg = yaml.load(cfg_file, Loader=yaml.FullLoader)
+    with open('configs/vgae.yaml', 'r') as cfg_file:
+        relation_engine_cfg = yaml.load(cfg_file, Loader = yaml.FullLoader)
+
+    inferencer = Inferencer(engine_cfg, relation_engine_cfg, engine = "citation_bert", relation_engine = "VGAE")
     
     print("127.0.0.1:10086 websocket...")
 
